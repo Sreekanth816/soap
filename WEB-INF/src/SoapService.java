08a9986048723d1c88e576c5cd13c14833065db1
Sreekanth Gundagoni <sreekanth.816@gmail.com>
2020-03-27 09:24:22 +0530

    Update SoapService.java

--- WEB-INF/src/SoapService.java
+++ WEB-INF/src/SoapService.java
@@ -2,13 +2,21 @@ package onjava;
 
 public class SoapService extends Object {
 
-    /** Creates new SoapService */
-    public SoapService() {
-    }
-    
-    /** the remote method accepting a Person object
-    */
-    public String sayGreeting(Person name) {
-        return "Hello dear "+name.getName()+" you are really "+name.getAge() + " and your address : " + name.getAddressObj().getAddress();
-    }
+	/** Creates new SoapService */
+	public SoapService() {
+	}
+
+	/**
+	 * the remote method accepting a Person object
+	 */
+	public int getSalary(Person name) {
+		System.out.println("Hello dear " + name.getName() + " you are really " + name.getAge() + " and your address : "
+				+ name.getAddress().getAddress());
+		return 5467;
+	}
+	
+	public float getTaxAmount(int salary) {
+		System.out.println("Salary: " + salary);
+		return salary / 123;
+	}
 }
